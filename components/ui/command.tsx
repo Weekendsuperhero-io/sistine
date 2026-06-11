720d02560ec98f0b13ad97ff43ed1adec1168290
Weekendsuperhero <4048475+WeekendSuperhero@users.noreply.github.com>
2026-06-11 03:41:42 -0700

    lint fixes

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -5,7 +5,7 @@ import { Command as CommandPrimitive } from "cmdk";
 import type { Dialog as DialogPrimitive } from "radix-ui";
 import * as React from "react";
 import { cn } from "@/lib/utils";
-import { Dialog, DialogContent } from "./dialog";
+import { Dialog, DialogContent, DialogTitle } from "./dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -39,6 +39,8 @@ const CommandDialog = ({ children, ...props }: DialogPrimitive.DialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
+        {/* Radix a11y: DialogContent requires a DialogTitle. */}
+        <DialogTitle className="sr-only">Command palette</DialogTitle>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
